b3b33ea2df22e04da58dd598b389e1eb07fb52e0
Ganga Nomika Mahesh <ganga.mahesh00@gmail.com>
2025-06-26 01:55:43 -0400

    First pass at templates and styling
    Added base.html and index.html, set up template inheritance, and linked a CSS file. Home route now shows the template.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grades.db'
 
 @app.route('/')
 def home():
-    return "Hello, World!"  # You can change this later to render an HTML page
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
